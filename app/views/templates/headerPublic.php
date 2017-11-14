52d54ba0ab99374b883f25e7ef2e2796ab37373c
muhammadsul <x6x@email.com>
2017-11-14 02:27:09 -0500

    More code added
    This commit to implement header and footer and bootstrap an orgnaized assingment 7

--- app/views/templates/headerPublic.php
+++ app/views/templates/headerPublic.php
@@ -16,3 +16,26 @@ if (isset($_SESSION['auth']) == 1) {
     <meta name="mobile-web-app-capable" content="yes">
 </head>
 <body>
+	 <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href='/login/register'>Sign up</a></li>
+            <li class="active"><a href="#">About</a></li>
+        </ul>
+
+	<footer id="page-footer">
+			<form class="nav navbar-nav navbar-right">
+				<div id="course-footer"></div>
+				<p class="helplink"></p>
+				<div class="footnote text-center"><div class="text_to_html"><p>COSC 4806</p></div></div></div></footer>
+			</form>
+			<form class="nav navbar-nav navbar-right">
+				<button type="button" class="btn btn-secondary" title="" data-container="body" data-toggle="popover" data-placement="top" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." data-original-title="Popover Title">Top</button>
+			</form>
+		</footer>
+</body>
+</html>
